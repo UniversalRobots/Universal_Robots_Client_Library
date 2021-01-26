ba9c4666c6da21e57792cfa268c0298bfd1cd161
Tristan Schnell <schnell@fzi.de>
2021-01-26 16:35:24 +0100

    added additional socket to send trajectory points

--- include/ur_client_library/ur/ur_driver.h
+++ include/ur_client_library/ur/ur_driver.h
@@ -230,6 +230,7 @@ private:
   comm::INotifier notifier_;
   std::unique_ptr<rtde_interface::RTDEClient> rtde_client_;
   std::unique_ptr<comm::ReverseInterface> reverse_interface_;
+  std::unique_ptr<comm::ReverseInterface> trajectory_point_reverse_interface_;
   std::unique_ptr<comm::ScriptSender> script_sender_;
   std::unique_ptr<comm::URStream<primary_interface::PrimaryPackage>> primary_stream_;
   std::unique_ptr<comm::URStream<primary_interface::PrimaryPackage>> secondary_stream_;
