26c588acaebf8045de8f67d78a93e1d89933ef9e
urmahp <mahp@universal-robots.com>
2022-08-12 12:30:58 +0200

    Added tcp_offset

--- src/rtde/data_package.cpp
+++ src/rtde/data_package.cpp
@@ -553,6 +553,7 @@ std::unordered_map<std::string, DataPackage::_rtde_type_variant> DataPackage::g_
   { "standard_analog_output_type", uint8_t() },
   { "standard_analog_output_0", double() },
   { "standard_analog_output_1", double() },
+  { "tcp_offset", vector6d_t() },
 };
 
 void rtde_interface::DataPackage::initEmpty()
