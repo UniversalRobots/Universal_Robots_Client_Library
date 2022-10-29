f43c61844b6e5dcb43d531f7c295496bdb169ff3
urrsk <41109954+urrsk@users.noreply.github.com>
2022-10-29 09:57:38 +0200

    Change to URSim docker image
    Instead of building one in the CI we now uses
    the image made by UR

--- tests/resources/dockerursim/build_and_run_docker_ursim.sh
+++ tests/resources/dockerursim/build_and_run_docker_ursim.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-URSIM_VERSION="${1:-5.8.0.10253}"
+URSIM_DOCKER_IMAGE=${1}
+PROGRAM_FOLDER=${2}
 
 docker network create --subnet=192.168.56.0/24 static_test_net
 
-docker build ${DIR} -t mydockerursim --build-arg VERSION="${URSIM_VERSION}"
-docker volume create dockerursim
-docker run --name="mydockerursim" -d \
-  -e ROBOT_MODEL=UR5 \
+docker run --rm -it -d \
+  --name ursim  \
   --net static_test_net \
   --ip 192.168.56.101 \
   -p 8080:8080 \
   -p 29999:29999 \
   -p 30001-30004:30001-30004 \
-  -v "${DIR}/.vol":/ursim/programs \
-  -v dockursim:/ursim \
+  -v "${DIR}/${PROGRAM_FOLDER}":/ursim/programs \
   --privileged \
   --cpus=1 \
-  mydockerursim
+  ${URSIM_DOCKER_IMAGE}
