ad89905acd63ebe363cc6d909b7a8adae7006277
Mads Holm Peters <79145214+urmahp@users.noreply.github.com>
2023-08-22 11:30:46 +0200

    Producer_fifo_scheduling should be set to true, such that it looks identical to the original constructor.

--- src/rtde/rtde_client.cpp
+++ src/rtde/rtde_client.cpp
@@ -56,7 +56,7 @@ RTDEClient::RTDEClient(std::string robot_ip, comm::INotifier& notifier, const st
   , input_recipe_(input_recipe)
   , parser_(output_recipe_)
   , prod_(stream_, parser_)
-  , pipeline_(prod_, PIPELINE_NAME, notifier)
+  , pipeline_(prod_, PIPELINE_NAME, notifier, true)
   , writer_(&stream_, input_recipe_)
   , max_frequency_(URE_MAX_FREQUENCY)
   , target_frequency_(target_frequency)
