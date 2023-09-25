052652ba60c3a9c77b96999b5adc7c4d9c10194b
Felix Exner (fexner) <exner@fzi.de>
2023-09-25 08:36:44 +0200

    Make depreaction warning for keepalive_counter in RevereseInterface a… (#183)
    … warning instead of error

--- src/control/reverse_interface.cpp
+++ src/control/reverse_interface.cpp
@@ -204,10 +204,10 @@ bool ReverseInterface::writeFreedriveControlMessage(const FreedriveControlMessag
 
 void ReverseInterface::setKeepaliveCount(const uint32_t count)
 {
-  URCL_LOG_ERROR("DEPRECATION NOTICE: Setting the keepalive count has been deprecated. Instead you should set the "
-                 "timeout directly in the write commands. Please change your code to set the read timeout in the write "
-                 "commands "
-                 "directly. This keepalive count will overwrite the timeout passed to the write functions.");
+  URCL_LOG_WARN("DEPRECATION NOTICE: Setting the keepalive count has been deprecated. Instead you should set the "
+                "timeout directly in the write commands. Please change your code to set the read timeout in the write "
+                "commands "
+                "directly. This keepalive count will overwrite the timeout passed to the write functions.");
   keepalive_count_ = count;
   keep_alive_count_modified_deprecated_ = true;
 }
