6a372083f6f90766440338e92356eddb35ca9b74
URJala <159417921+URJala@users.noreply.github.com>
2024-10-08 11:06:32 +1100

    Change damping factor values in example
    Co-authored-by: Felix Exner (fexner) <felix_mauch@web.de>

--- examples/force_mode_example.cpp
+++ examples/force_mode_example.cpp
@@ -186,7 +186,7 @@ int main(int argc, char* argv[])
                                           { 0, 0, 0, 0, 0, 0 },  // do not apply any active wrench
                                           2,                     // do not transform the force frame at all
                                           { 0.1, 0.1, 1.5, 3.14, 3.14, 0.5 },  // limits
-                                          0.8);  // damping_factor. See ScriptManual for details.
+                                          0.025);  // damping_factor. See ScriptManual for details.
   else
   {
     success = g_my_driver->startForceMode({ 0, 0, 0, 0, 0, 0 },  // Task frame at the robot's base
@@ -194,7 +194,7 @@ int main(int argc, char* argv[])
                                           { 0, 0, 0, 0, 0, 0 },  // do not apply any active wrench
                                           2,                     // do not transform the force frame at all
                                           { 0.1, 0.1, 1.5, 3.14, 3.14, 0.5 },  // limits
-                                          0.8,                                 // damping_factor
+                                          0.025,                                 // damping_factor
                                           0.8);  // gain_scaling. See ScriptManual for details.
   }
   if (!success)
