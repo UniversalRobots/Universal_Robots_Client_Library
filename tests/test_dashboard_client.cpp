726f48564d8065e68ec7d823cc99a5f4af737e7b
Felix Exner <feex@universal-robots.com>
2025-01-23 09:24:33 +0100

    Re-use existing dashboard client

--- tests/test_dashboard_client.cpp
+++ tests/test_dashboard_client.cpp
@@ -221,29 +221,25 @@ TEST_F(DashboardClientTest, connect_non_running_robot)
 
 TEST_F(DashboardClientTest, non_expected_result_returns_correctly)
 {
-  std::unique_ptr<DashboardClient> dashboard_client;
-  dashboard_client.reset(new DashboardClient("192.168.56.101"));
-  ASSERT_TRUE(dashboard_client->connect());
-  EXPECT_TRUE(dashboard_client->commandPowerOff());
+  ASSERT_TRUE(dashboard_client_->connect());
+  EXPECT_TRUE(dashboard_client_->commandPowerOff());
 
   // We will not get this answer
-  EXPECT_FALSE(dashboard_client->sendRequest("brake release", "non-existing-response"));
+  EXPECT_FALSE(dashboard_client_->sendRequest("brake release", "non-existing-response"));
 
   // A non-matching answer should throw an exception for this call
-  EXPECT_THROW(dashboard_client->sendRequestString("brake release", "non-existing-response"), UrException);
+  EXPECT_THROW(dashboard_client_->sendRequestString("brake release", "non-existing-response"), UrException);
 
   // Waiting for a non-matching answer should return false
   // Internally we wait 100ms between each attempt, hence the 300ms wait time
   EXPECT_FALSE(
-      dashboard_client->waitForReply("brake_release", "non-existing-response", std::chrono::milliseconds(300)));
+      dashboard_client_->waitForReply("brake_release", "non-existing-response", std::chrono::milliseconds(300)));
 }
 
-TEST_F(DashboardClientTest, connecting_twoice_returns_false)
+TEST_F(DashboardClientTest, connecting_twice_returns_false)
 {
-  std::unique_ptr<DashboardClient> dashboard_client;
-  dashboard_client.reset(new DashboardClient("192.168.56.101"));
-  ASSERT_TRUE(dashboard_client->connect());
-  EXPECT_FALSE(dashboard_client->connect());
+  ASSERT_TRUE(dashboard_client_->connect());
+  EXPECT_FALSE(dashboard_client_->connect());
 }
 
 int main(int argc, char* argv[])
