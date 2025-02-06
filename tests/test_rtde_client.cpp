316b1b30abb976a36ff9ac6e84fdcd9fb50bf4d8
Felix Exner <feex@universal-robots.com>
2025-02-06 13:06:06 +0100

    RTDEClient: pause and stop in destructor only if running (#257)
    Otherwise we produce an error when destructing an initialized, but
    non-started client.

--- tests/test_rtde_client.cpp
+++ tests/test_rtde_client.cpp
@@ -380,7 +380,7 @@ TEST_F(RTDEClientTest, check_all_rtde_output_variables_exist)
   client_.reset(new rtde_interface::RTDEClient(g_ROBOT_IP, notifier_, exhaustive_output_recipe_file_,
                                                input_recipe_file_, 0.0, true));
 
-  EXPECT_NO_THROW(client_->init());
+  EXPECT_TRUE(client_->init());
   client_->start();
 
   // Test that we can receive and parse the timestamp from the received package to prove the setup was successful
