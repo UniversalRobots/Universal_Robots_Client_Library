1bf825c7ecf94331577fb86ecc157e61ccd03dee
Felix Exner <feex@universal-robots.com>
2025-02-13 10:53:50 +0100

    Update include/ur_client_library/primary/primary_client.h
    Co-authored-by: Dominic Reber <71256590+domire8@users.noreply.github.com>

--- include/ur_client_library/primary/primary_client.h
+++ include/ur_client_library/primary/primary_client.h
@@ -66,8 +66,8 @@ public:
    * \param primary_consumer Primary consumer that should be removed from the list
    */
   void removePrimaryConsumer(std::shared_ptr<comm::IConsumer<PrimaryPackage>> primary_consumer);
-  void start(const size_t max_num_tries = 0,
-             const std::chrono::milliseconds reconnection_time = urcl::comm::TCPSocket::DEFAULT_RECONNECTION_TIME);
+  void start(const size_t max_connection_attempts = 0,
+             const std::chrono::milliseconds reconnection_timeout = urcl::comm::TCPSocket::DEFAULT_RECONNECTION_TIME);
 
   /*!
    * \brief Retrieves previously raised error codes from PrimaryClient. After calling this, recorded errors will be
