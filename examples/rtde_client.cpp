b709e8c8b721a0db290784842152dbabe0ec5e39
Felix Exner <feex@universal-robots.com>
2026-01-22 10:41:03 +0100

    Update RTDEClient example documentation

--- examples/rtde_client.cpp
+++ examples/rtde_client.cpp
@@ -83,28 +83,26 @@ int main(int argc, char* argv[])
   double target_speed_fraction = 1.0;
   double speed_slider_increment = 0.01;
 
+  std::unique_ptr<rtde_interface::DataPackage> data_pkg =
+      std::make_unique<rtde_interface::DataPackage>(my_client.getOutputRecipe());
   // Once RTDE communication is started, we have to make sure to read from the interface buffer, as
   // otherwise we will get pipeline overflows. Therefor, do this directly before starting your main
   // loop.
   my_client.start(false);
-  std::unique_ptr<rtde_interface::DataPackage> data_pkg =
-      std::make_unique<rtde_interface::DataPackage>(my_client.getOutputRecipe());
 
   auto start_time = std::chrono::steady_clock::now();
   while (second_to_run <= 0 ||
          std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start_time).count() <
              second_to_run)
   {
-    // Read latest RTDE package. This will block for READ_TIMEOUT, so the
-    // robot will effectively be in charge of setting the frequency of this loop unless RTDE
-    // communication doesn't work in which case the user will be notified.
-    // In a real-world application this thread should be scheduled with real-time priority in order
-    // to ensure that this is called in time.
+    // Wait for a DataPackage. In a real-world application this thread should be scheduled with real-time priority in
+    // order to ensure that this is called in time.
     bool success = my_client.getDataPackageBlocking(data_pkg);
     if (success)
     {
       // Data fields in the data package are accessed by their name. Only names present in the
       // output recipe can be accessed. Otherwise this function will return false.
+      // We preallocated the string TARGET_SPEED_FRACTION to avoid allocations in the main loop.
       data_pkg->getData(TARGET_SPEED_FRACTION, target_speed_fraction);
       printFraction(target_speed_fraction, TARGET_SPEED_FRACTION);
     }
