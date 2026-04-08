7a743c8d3b29bf7aba5759e5e96219b7361edf99
Jacob Larsen <jala@universal-robots.com>
2026-04-08 14:08:00 +0000

    Example (very wip)

--- examples/primary_client.cpp
+++ examples/primary_client.cpp
@@ -3,18 +3,21 @@
 
 int main()
 {
+  using namespace std::chrono_literals;
   auto notif = urcl::comm::INotifier();
   auto client = urcl::primary_interface::PrimaryClient("192.168.56.101", notif);
   client.start(10);
-  // std::cout << "Robot mode: " << int(client.getRobotMode()) << std::endl;
-  // client.commandPowerOff();
-  // std::cout << "Robot mode: " << int(client.getRobotMode()) << std::endl;
-  // client.commandBrakeRelease();
-  // std::cout << "Robot mode: " << int(client.getRobotMode()) << std::endl;
-  auto s = "textmsg(\"hello\") \n #hhelllooe \n \n #helloe \n #jejfef";
-  client.sendScript(s, "hello_world", urcl::primary_interface::ScriptTypes::SEC);
+  std::cout << "Client connected" << std::endl;
+  // std::this_thread::sleep_for(3000ms);
+  //  std::cout << "Robot mode: " << int(client.getRobotMode()) << std::endl;
+  //  client.commandPowerOff();
+  //  std::cout << "Robot mode: " << int(client.getRobotMode()) << std::endl;
+  //  client.commandBrakeRelease();
+  //  std::cout << "Robot mode: " << int(client.getRobotMode()) << std::endl;
+  auto s = "movej([1,0,0,0,0,0], t=5)";
+  client.sendScript(s, "");
 
-  client.sendScript(s, "", urcl::primary_interface::ScriptTypes::DEF);
-  using namespace std::chrono_literals;
-  std::this_thread::sleep_for(2000ms);
+  // client.sendScript(s, "", urcl::primary_interface::ScriptTypes::DEF);
+
+  // std::this_thread::sleep_for(2000ms);
 }
