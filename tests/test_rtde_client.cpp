135e4239dcd086d7b9c1135fc1384fe147b46e8d
Felix Exner <feex@universal-robots.com>
2026-04-28 11:47:54 +0200

    Make downloading and checking RTDE outputs from docs optional
    It's not particularly nice to require internet access to build the
    project every time.
    Also, this reduces build requirements to run integration tests so we
    don't need python3 and pandas to run integration tests.

--- tests/test_rtde_client.cpp
+++ tests/test_rtde_client.cpp
@@ -681,6 +681,7 @@ TEST_F(RTDEClientTest, check_all_rtde_output_variables_exist)
   client_->pause();
 }
 
+#ifdef CHECK_RTDE_DOCS_RECIPE
 TEST_F(RTDEClientTest, check_rtde_data_fields_match_docs)
 {
   std::ifstream docs_file(docs_output_recipe_file_);
@@ -725,6 +726,7 @@ TEST_F(RTDEClientTest, check_rtde_data_fields_match_docs)
     GTEST_FAIL();
   }
 }
+#endif
 
 TEST_F(RTDEClientTest, check_unknown_rtde_output_variable)
 {
