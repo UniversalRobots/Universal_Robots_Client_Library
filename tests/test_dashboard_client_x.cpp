d5ac556be99a9381c15fe2b9a095dc55bc0c9c49
Felix Exner <feex@universal-robots.com>
2026-08-21 16:15:24 +0200

    Fix comparison

--- tests/test_dashboard_client_x.cpp
+++ tests/test_dashboard_client_x.cpp
@@ -603,25 +603,24 @@ TEST_F(DashboardClientTestX, get_robot_model)
   {
     auto response = dashboard_client_->commandGetRobotModel();
     ASSERT_TRUE(response.ok);
-    const std::string model_string = std::get<std::string>(response.data["robot_model"]);
+    std::string model_string = std::get<std::string>(response.data["robot_model"]);
 
     waitFor([this]() { return primary_client_->getRobotType() != urcl::RobotType::UNDEFINED; },
             std::chrono::milliseconds(1000));
 
     const std::string primary_client_version = robotTypeString(primary_client_->getRobotType());
-    std::string expected_model_string = primary_client_version;
 
     // On the primary interface UR7 and UR12 show as UR5 and UR10, so we need to adjust the
     // expected value accordingly.
     if (model_string == "UR7")
     {
-      expected_model_string = "UR5";
+      model_string = "UR5";
     }
     else if (model_string == "UR12")
     {
-      expected_model_string = "UR10";
+      model_string = "UR10";
     }
-    EXPECT_EQ(model_string, expected_model_string);
+    EXPECT_EQ(model_string, primary_client_version);
   }
 }
 
